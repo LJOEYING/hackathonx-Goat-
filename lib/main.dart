7bec416dd823a80fd2d89ce58f6e1a57f5e85acb
LJOEYING <joeyingloh0326@gmail.com>
2024-10-22 21:42:34 +0800

    change main

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       title: 'Waste Management & Car Pooling App',
       debugShowCheckedModeBanner: false,
-      home: StartedScreen(),
+      home: BottomAppBar(),
     );
   }
 }
