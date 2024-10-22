723454d10fe66803f072d5f43072a4c1e6960f32
LJOEYING <joeyingloh0326@gmail.com>
2024-10-22 18:09:15 +0800

    update nav bar

--- lib/widgets/bottom_nav_page.dart
+++ lib/widgets/bottom_nav_page.dart
@@ -1,3 +1,4 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:hackathonx/pages/car_pooling.dart';
 import 'package:hackathonx/pages/green_map.dart';
@@ -26,103 +27,142 @@ class _BottomNavPageState extends State<BottomNavPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
+
       body: pages[myCurrentPage],
-      floatingActionButton: Container(
-        width: 60,
-        height: 80,
-        child: FloatingActionButton(
-          onPressed: () {
+      bottomNavigationBar: CurvedNavigationBar(
+          color: Colors.black,
+          backgroundColor: Colors.transparent,
+          index: myCurrentPage,
+          onTap: (index) {
             setState(() {
-              myCurrentPage = 2;
+              myCurrentPage = index;
             });
           },
-          backgroundColor: Colors.black,
-          shape: CircleBorder(),
-          child: Icon(
-            Icons.qr_code_scanner_sharp,
-            size: 35,
-            color: myCurrentPage == 2 ? Colors.white : Colors.grey,
-          ),
-        ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      bottomNavigationBar: BottomAppBar(
-        color: Colors.black,
-        // surfaceTintColor: Colors.red,
-        shape: CircularNotchedRectangle(),
-        notchMargin: 8,
-        child: Container(
-          // height: 30,
-          // width: 20,
-          // color: Colors.transparent,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  MaterialButton(
-                    // minWidth: 70,
-                    onPressed: () {
-                      setState(() {
-                        myCurrentPage = 0;
-                      });
-                    },
-                    child: Icon(
-                      Icons.home,
-                      size: 35,
-                      color: myCurrentPage == 0 ? Colors.white : Colors.grey,
-                    ),
-                  ),
-                  MaterialButton(
-                    // minWidth: 70,
-                    onPressed: () {
-                      setState(() {
-                        myCurrentPage = 1;
-                      });
-                    },
-                    child: Icon(
-                      Icons.location_on,
-                      size: 35,
-                      color: myCurrentPage == 1 ? Colors.white : Colors.grey,
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  MaterialButton(
-                    // minWidth: 70,
-                    onPressed: () {
-                      setState(() {
-                        myCurrentPage = 3;
-                      });
-                    },
-                    child: Icon(
-                      Icons.perm_device_information_outlined,
-                      size: 35,
-                      color: myCurrentPage == 3 ? Colors.white : Colors.grey,
-                    ),
-                  ),
-                  MaterialButton(
-                    // minWidth: 70,
-                    onPressed: () {
-                      setState(() {
-                        myCurrentPage = 4;
-                      });
-                    },
-                    child: Icon(
-                      Icons.car_crash,
-                      size: 35,
-                      color: myCurrentPage == 4 ? Colors.white : Colors.grey,
-                    ),
-                  ),
-                ],
-              )
-            ],
-          ),
-        ),
-      ),
+          items: [
+            Icon(
+              Icons.home,
+              size: 32,
+              color: Colors.white,
+            ),
+            Icon(
+              Icons.location_on,
+              size: 32,
+              color: Colors.white,
+            ),
+            Icon(
+              Icons.qr_code_scanner_sharp,
+              size: 32,
+              color: Colors.white,
+            ),
+            Icon(
+              Icons.perm_device_information_rounded,
+              size: 32,
+              color: Colors.white,
+            ),
+            Icon(
+              Icons.car_crash,
+              size: 32,
+              color: Colors.white,
+            ),
+          ]),
+
+      // floatingActionButton: Container(
+      //   width: 60,
+      //   height: 80,
+      //   child: FloatingActionButton(
+      //     onPressed: () {
+      //       setState(() {
+      //         myCurrentPage = 2;
+      //       });
+      //     },
+      //     backgroundColor: Colors.black,
+      //     shape: CircleBorder(),
+      //     child: Icon(
+      //       Icons.qr_code_scanner_sharp,
+      //       size: 35,
+      //       color: myCurrentPage == 2 ? Colors.white : Colors.grey,
+      //     ),
+      //   ),
+      // ),
+      // floatingActionButtonLocation:
+      // FloatingActionButtonLocation.centerDocked,
+      // bottomNavigationBar: BottomAppBar(
+      //   color: Colors.black,
+      //   // surfaceTintColor: Colors.red,
+      //   shape: CircularNotchedRectangle(),
+      //   notchMargin: 8,
+      //   child: Container(
+      //     // height: 30,
+      //     // width: 20,
+      //     // color: Colors.transparent,
+      //     child: Row(
+      //       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      //       children: [
+      //         Row(
+      //           mainAxisAlignment: MainAxisAlignment.center,
+      //           children: [
+      //             MaterialButton(
+      //               // minWidth: 70,
+      //               onPressed: () {
+      //                 setState(() {
+      //                   myCurrentPage = 0;
+      //                 });
+      //               },
+      //               child: Icon(
+      //                 Icons.home,
+      //                 size: 35,
+      //                 color: myCurrentPage == 0 ? Colors.white : Colors.grey,
+      //               ),
+      //             ),
+      //             MaterialButton(
+      //               // minWidth: 70,
+      //               onPressed: () {
+      //                 setState(() {
+      //                   myCurrentPage = 1;
+      //                 });
+      //               },
+      //               child: Icon(
+      //                 Icons.location_on,
+      //                 size: 35,
+      //                 color: myCurrentPage == 1 ? Colors.white : Colors.grey,
+      //               ),
+      //             ),
+      //           ],
+      //         ),
+      //         Row(
+      //           children: [
+      //             MaterialButton(
+      //               // minWidth: 70,
+      //               onPressed: () {
+      //                 setState(() {
+      //                   myCurrentPage = 3;
+      //                 });
+      //               },
+      //               child: Icon(
+      //                 Icons.perm_device_information_outlined,
+      //                 size: 35,
+      //                 color: myCurrentPage == 3 ? Colors.white : Colors.grey,
+      //               ),
+      //             ),
+      //             MaterialButton(
+      //               // minWidth: 70,
+      //               onPressed: () {
+      //                 setState(() {
+      //                   myCurrentPage = 4;
+      //                 });
+      //               },
+      //               child: Icon(
+      //                 Icons.car_crash,
+      //                 size: 35,
+      //                 color: myCurrentPage == 4 ? Colors.white : Colors.grey,
+      //               ),
+      //             ),
+      //           ],
+      //         )
+      //       ],
+      //     ),
+      //   ),
+      // ),
     );
   }
 }
