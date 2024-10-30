c7da5e0870a9e0fafe5b061c731d4818ad839d75
LJOEYING <joeyingloh0326@gmail.com>
2024-10-30 23:06:21 +0800

    update_2

--- lib/pages/green_map.dart
+++ lib/pages/green_map.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 //display the map and collection type selector
 class GreenMap extends StatelessWidget {
   @override
@@ -28,7 +29,7 @@ class GreenMap extends StatelessWidget {
           ),
           //display the collection type selector
           Positioned(
-            bottom: -20,
+            bottom: -15,
             left: 0,
             right: 0,
             child: CollectionTypeSelector(),
@@ -38,55 +39,65 @@ class GreenMap extends StatelessWidget {
     );
   }
 }
+
 //represent a search input field
 class SearchBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.symmetric(horizontal: 10),
-      color: Colors.white,
       child: TextField(
         decoration: InputDecoration(
-          prefixIcon: Icon(Icons.search),
+          prefixIcon: Icon(
+            Icons.search,
+            color: Colors.white,
+          ),
           hintText: 'Search anywhere nearby...',
+          hintStyle: TextStyle(color: Colors.white),
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(30),
+            borderSide:
+                BorderSide.none, // Remove the border to match the container
           ),
+          filled: true, // This ensures that the background color is applied
+          fillColor: Colors.black, // Set the fill color for the TextField
         ),
       ),
     );
   }
 }
+
 //display various types of waste collection options
 class CollectionTypeSelector extends StatelessWidget {
   final items = [
     {
-      'label': 'Dustbins',//label for the dustbin
+      'label': 'Dustbins', //label for the dustbin
       'photo': 'assets/dustbin.png',
       'color': Colors.red[200]
     },
     {
-      'label': 'Recycle Bins',//label for the recycle bin option
+      'label': 'Recycle Bins', //label for the recycle bin option
       'photo': 'assets/recycle_bin.png',
       'color': Colors.orange[300]
     },
     {
-      'label': 'Communal Bins',//label for the communal bin option
+      'label': 'Communal Bins', //label for the communal bin option
       'photo': 'assets/communal_bin.png',
       'color': Colors.yellow[300]
     },
     {
-      'label': 'Collection Centres',//label for the collection centre option
+      'label': 'Collection Centres', //label for the collection centre option
       'photo': 'assets/collection_center.png',
       'color': Colors.green[200]
     },
     {
-      'label': 'Recycle Centres',//label for the recycle centre option
+      'label': 'Recycle Centres', //label for the recycle centre option
       'photo': 'assets/recycle_center.png',
       'color': Colors.blue[200]
     },
     {
-      'label': 'Mobile Recycling Centres',//label for the mobile recycling centre option
+      'label':
+          'Mobile Recycling Centres', //label for the mobile recycling centre option
       'photo': 'assets/mobile_recycle_center.png',
       'color': Colors.purple[200]
     },
@@ -107,15 +118,18 @@ class CollectionTypeSelector extends StatelessWidget {
           childAspectRatio: 1,
         ),
         itemBuilder: (context, index) {
-          final label = items[index]['label']! as String;//get the label
-          final photo = items[index]['photo']! as String;//get the image path
-          final color = items[index]['color']! as Color;// get the background color
-          return _buildCollectionIcon(label, photo, color);// build the icon for the item
+          final label = items[index]['label']! as String; //get the label
+          final photo = items[index]['photo']! as String; //get the image path
+          final color =
+              items[index]['color']! as Color; // get the background color
+          return _buildCollectionIcon(
+              label, photo, color); // build the icon for the item
         },
       ),
     );
   }
-//build waste collection 
+
+//build waste collection
   Widget _buildCollectionIcon(String label, String photo, Color color) {
     return Column(
       mainAxisSize: MainAxisSize.min,
@@ -124,14 +138,15 @@ class CollectionTypeSelector extends StatelessWidget {
           height: 60,
           width: 60,
           decoration: BoxDecoration(
-            color: color,//set the background color for the waste collection type
+            color:
+                color, //set the background color for the waste collection type
             shape: BoxShape.rectangle,
             borderRadius: BorderRadius.circular(20),
           ),
           child: Padding(
             padding: const EdgeInsets.all(8.0),
             child: Image.asset(
-              photo,//load the image from assets
+              photo, //load the image from assets
               fit: BoxFit.contain,
               height: 45,
               width: 45,
@@ -140,7 +155,7 @@ class CollectionTypeSelector extends StatelessWidget {
         ),
         SizedBox(height: 8),
         Text(
-          label,//display the label for the waste collection type
+          label, //display the label for the waste collection type
           textAlign: TextAlign.center,
           maxLines: 2,
           overflow: TextOverflow.ellipsis,
